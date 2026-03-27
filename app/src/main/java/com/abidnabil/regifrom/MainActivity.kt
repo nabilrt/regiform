720c3fc1b6fb9960f8445bad6d21c0ee258cc65f
Abidur Rahman Nabil <nabilrt51@gmail.com>
2026-03-28 01:52:14 +0600

    material button use

--- app/src/main/java/com/abidnabil/regifrom/MainActivity.kt
+++ app/src/main/java/com/abidnabil/regifrom/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var emailTextBox: TextInputEditText
     private lateinit var phoneTextBox: TextInputEditText
     private lateinit var passwordTextBox: TextInputEditText
-    private lateinit var submitButton: Button
+    private lateinit var submitButton: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
